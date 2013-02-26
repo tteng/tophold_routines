8b6faae7a3be3b328cd568acc68482dda8910141
readline <tim.rubist@gmail.com>
2013-02-26 11:21:48 +0800

    fix undefined proxy_type bug

--- app/crawlers/proxies/vpn_geeks.rb
+++ app/crawlers/proxies/vpn_geeks.rb
@@ -16,7 +16,6 @@ class VpnGeeks
 
   def grab_proxies page_obj
     proxy_rows = page_obj.search "//tr[@class='tr_style1' or @class='tr_style2']"
-    p proxy_rows.size
     proxy_rows.to_a.each do |row|
       parse_each_row row
     end
@@ -24,8 +23,8 @@ class VpnGeeks
 
   def parse_each_row row
     children = row.children
+    ip, port, country, proxy_type = children[0].text, children[1].text, children[2].text, children[8].text
     return unless proxy_type =~ /http/i
-    ip, port, country = children[0].text, children[1].text, children[2].text
     speed = children[6].inner_html =~ /width\:(.*)%/ ? $1.gsub(/['"]/,'').to_i : 50
     STDOUT.puts({ip: ip, port: port, speed: speed, provider: country})
     HttpProxy.create({ip: ip, port: port, speed: speed, provider: country})
