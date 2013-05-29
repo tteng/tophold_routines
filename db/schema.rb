22536e7d3c5e0fb173187c52388d668e2e62f546
readline <tim.rubist@gmail.com>
2013-05-29 14:39:06 +0800

    1, modify pull attributes, only get price related changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130504075440) do
+ActiveRecord::Schema.define(:version => 20130529034936) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(:version => 20130504075440) do
     t.float    "book_value"
     t.datetime "created_at",                                    :null => false
     t.datetime "updated_at",                                    :null => false
+    t.float    "schange"
+    t.string   "change_in_percent",               :limit => 10
   end
 
   create_table "stocks", :force => true do |t|
